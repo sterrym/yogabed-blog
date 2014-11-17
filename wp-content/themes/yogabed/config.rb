af59de1f642b36d67fec7ee0dae2dd9413595535
Daniel Robinson <d.robinson004@gmail.com>
2014-11-17 13:09:02 -0500

    included goran theme

--- wp-content/themes/yogabed/config.rb
+++ wp-content/themes/yogabed/config.rb
@@ -1,4 +1,13 @@
-css_dir = ""
+css_dir = "css"
 sass_dir = "sass"
+
 output_style = (environment == :production) ? :compressed : :expanded
 line_comments = (environment == :production) ? :false : :true
+
+require 'fileutils'
+on_stylesheet_saved do |file|
+  if File.exists?(file) && File.basename(file) == "style.css"
+    puts "Moving: #{file}"
+    FileUtils.mv(file, File.dirname(file) + "/../" + File.basename(file))
+  end
+end

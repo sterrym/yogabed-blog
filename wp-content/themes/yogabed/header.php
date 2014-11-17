b9e7011cb17370210c5e8063e6a04723cc6edcd0
Daniel Robinson <d.robinson004@gmail.com>
2014-11-17 12:36:42 -0500

    small nav change

--- wp-content/themes/yogabed/header.php
+++ wp-content/themes/yogabed/header.php
@@ -23,33 +23,35 @@
   <a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'goran' ); ?></a>
 
   <header id="masthead" class="site-header" role="banner">
-    <?php
-      $top_area_content = get_theme_mod( 'goran_top_area_content' );
-      if ( '' != $top_area_content ) :
-    ?>
-    <div class="site-top-content">
-      <?php echo $top_area_content; ?>
-    </div><!-- .site-top-content -->
-    <?php endif; ?>
+    <div class="header-wrapper clear">
+      <?php
+        $top_area_content = get_theme_mod( 'goran_top_area_content' );
+        if ( '' != $top_area_content ) :
+      ?>
+      <div class="site-top-content">
+        <?php echo $top_area_content; ?>
+      </div><!-- .site-top-content -->
+      <?php endif; ?>
 
-    <div class="site-branding">
-      <?php edin_the_site_logo(); ?>
-      <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-      <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-    </div><!-- .site-branding -->
+      <div class="site-branding">
+        <?php edin_the_site_logo(); ?>
+        <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+        <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+      </div><!-- .site-branding -->
 
-    <?php if ( has_nav_menu( 'primary' ) ) : ?>
-      <nav id="site-navigation" class="main-navigation" role="navigation">
-        <button class="menu-toggle"><?php _e( 'Menu', 'goran' ); ?></button>
-        <?php
-          wp_nav_menu( array(
-            'theme_location'  => 'primary',
-            'container_class' => 'menu-primary',
-            'menu_class'      => 'clear',
-          ) );
-        ?>
-      </nav><!-- #site-navigation -->
-    <?php endif; ?>
+      <?php if ( has_nav_menu( 'primary' ) ) : ?>
+        <nav id="site-navigation" class="main-navigation" role="navigation">
+          <button class="menu-toggle"><?php _e( 'Menu', 'goran' ); ?></button>
+          <?php
+            wp_nav_menu( array(
+              'theme_location'  => 'primary',
+              'container_class' => 'menu-primary',
+              'menu_class'      => 'clear',
+            ) );
+          ?>
+        </nav><!-- #site-navigation -->
+      <?php endif; ?>
+    </div>
   </header><!-- #masthead -->
 
   <div id="content" class="site-content">

6ea1bfa15e9e51a91fda315a81d453e5b0e824b6
Daniel Robinson <d.robinson004@gmail.com>
2014-11-18 14:04:34 -0500

    adding bootstrap js

--- wp-content/themes/yogabed/functions.php
+++ wp-content/themes/yogabed/functions.php
@@ -4,3 +4,8 @@ add_action( 'wp_enqueue_scripts', 'enqueue_parent_theme_style');
 function enqueue_parent_theme_style() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
+
+add_action( 'wp_enqueue_scripts', 'enqueue_bootstrap_javascript' );
+function enqueue_bootstrap_javascript() {
+  wp_enqueue_script( 'bootstrap', get_stylesheet_directory_uri().'/javascripts/bootstrap.js' );
+}

737f30298b5a180793b205abd747325ce6328cfa
Daniel Robinson <d.robinson004@gmail.com>
2014-11-18 12:46:50 -0500

    removing old edin style

--- wp-content/themes/yogabed/footer.php
+++ wp-content/themes/yogabed/footer.php
@@ -12,16 +12,14 @@
 
   <?php get_sidebar( 'footer' ); ?>
 
-  <footer id="footer" role="contentinfo">
-    <div class="container">
-      <div class="legal">
-        <div class="container">
-          <div class="row">
-            <div class="col-sm-2 link">COPYRIGHT © 2014</div>
-            <div class="col-sm-2 link"><a href="https://yogabed-staging.herokuapp.com/privacy-policy">PRIVACY</a></div>
-            <div class="col-sm-3 link"><a href="https://yogabed-staging.herokuapp.com/shipping-return-warranty">SHIPPING, RETURNS &amp; WARRANTY</a></div>
-            <div class="col-sm-5 text-right"><a href="#top" class="return-to-top" title="Return to Top"><span class="fa fa-chevron-up"></span></a></div>
-          </div>
+  <footer id="lower-footer" role="contentinfo">
+    <div class="legal">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-2 link">COPYRIGHT © 2014</div>
+          <div class="col-sm-2 link"><a href="https://yogabed-staging.herokuapp.com/privacy-policy">PRIVACY</a></div>
+          <div class="col-sm-3 link"><a href="https://yogabed-staging.herokuapp.com/shipping-return-warranty">SHIPPING, RETURNS &amp; WARRANTY</a></div>
+          <div class="col-sm-5 text-right"><a href="#top" class="return-to-top" title="Return to Top"><span class="fa fa-chevron-up"></span></a></div>
         </div>
       </div>
     </div>

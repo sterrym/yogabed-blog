9c79ba9e6afe4f86ee6cce0bd93d356d2a4ad2b3
Daniel Robinson <d.robinson004@gmail.com>
2014-11-18 12:48:44 -0500

    adding back in edin styles

--- wp-content/themes/yogabed/functions.php
+++ wp-content/themes/yogabed/functions.php
@@ -2,5 +2,5 @@
 
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_theme_style');
 function enqueue_parent_theme_style() {
-    // wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }

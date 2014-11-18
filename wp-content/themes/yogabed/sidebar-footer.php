162e856f08479dbf4d8a79198493531f061fd4bc
Daniel Robinson <d.robinson004@gmail.com>
2014-11-18 10:22:52 -0500

    small change

--- wp-content/themes/yogabed/sidebar-footer.php
+++ wp-content/themes/yogabed/sidebar-footer.php
@@ -17,13 +17,13 @@
         <div class="col-xs-5 col-sm-4">
           <div class="row">
             <?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
-              <div class="footer-widget col-sm-6">
+              <div class="col-sm-6">
                 <?php dynamic_sidebar( 'sidebar-2' ); ?>
               </div><!-- .footer-widget -->
             <?php endif; ?>
 
             <?php if ( is_active_sidebar( 'sidebar-3' ) ) : ?>
-              <div class="footer-widget col-sm-6">
+              <div class="col-sm-6">
                 <?php dynamic_sidebar( 'sidebar-3' ); ?>
               </div><!-- .footer-widget -->
             <?php endif; ?>
@@ -35,14 +35,14 @@
             <?php endif; ?>
           </div>
         </div>
-      </div>
-      <div class="col-sm-offset-5 col-sm-3 col-xs-7" style="text-align:right">
-        <p>
-          <span><a href="http://instagram.com/yogabed" target="_blank"><img alt="Social instagram" src="<?php echo get_stylesheet_directory_uri(); ?>/images/social-instagram.png"></a></span> &nbsp; 
-          <span><a href="https://twitter.com/yogabedmattress" target="_blank"><img alt="Social twitter" src="<?php echo get_stylesheet_directory_uri(); ?>/images/social-twitter.png"></a></span>&nbsp;
-          <span><a href="https://facebook.com/yogabedmattress" target="_blank"><img alt="Social facebook" src="<?php echo get_stylesheet_directory_uri(); ?>/images/social-facebook.png"></a></span>
-        </p>
-        <p class="customer-service" style="font-size:20px;">Customer Service<br><a href="tel:+18009861656"><strong>1-800-986-1656</strong></a></p>
+        <div class="col-sm-offset-5 col-sm-3 col-xs-7" style="text-align:right">
+          <p>
+            <span><a href="http://instagram.com/yogabed" target="_blank"><img alt="Social instagram" src="<?php echo get_stylesheet_directory_uri(); ?>/images/social-instagram.png"></a></span> &nbsp; 
+            <span><a href="https://twitter.com/yogabedmattress" target="_blank"><img alt="Social twitter" src="<?php echo get_stylesheet_directory_uri(); ?>/images/social-twitter.png"></a></span>&nbsp;
+            <span><a href="https://facebook.com/yogabedmattress" target="_blank"><img alt="Social facebook" src="<?php echo get_stylesheet_directory_uri(); ?>/images/social-facebook.png"></a></span>
+          </p>
+          <p class="customer-service" style="font-size:20px;">Customer Service<br><a href="tel:+18009861656"><strong>1-800-986-1656</strong></a></p>
+        </div>
       </div>
     </div><!-- .footer-widget-wrapper -->
   </div><!-- #tertiary -->

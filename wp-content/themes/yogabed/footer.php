e5bee8069ee699fbe05ff5397323cf4dc7059f98
Daniel Robinson <d.robinson004@gmail.com>
2014-11-18 13:47:21 -0500

    trying to fix bootstrap fonts

--- wp-content/themes/yogabed/footer.php
+++ wp-content/themes/yogabed/footer.php
@@ -19,7 +19,7 @@
           <div class="col-sm-2 link">COPYRIGHT © 2014</div>
           <div class="col-sm-2 link"><a href="https://yogabed-staging.herokuapp.com/privacy-policy">PRIVACY</a></div>
           <div class="col-sm-3 link"><a href="https://yogabed-staging.herokuapp.com/shipping-return-warranty">SHIPPING, RETURNS &amp; WARRANTY</a></div>
-          <div class="col-sm-5 text-right"><a href="#top" class="return-to-top" title="Return to Top"><span class="fa fa-chevron-up"></span></a></div>
+          <div class="col-sm-5 text-right"><a href="#top" class="return-to-top" title="Return to Top"><span class="fa fa-chevron-up glyphicon-chevron-up  "></span></a></div>
         </div>
       </div>
     </div>

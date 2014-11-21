f762453b3fb9292556eea4a9e6e179d101e9afcd
Daniel Robinson <d.robinson004@gmail.com>
2014-11-21 11:39:45 -0500

    adding google analytics and google conversion tags

--- wp-content/themes/yogabed/footer.php
+++ wp-content/themes/yogabed/footer.php
@@ -28,5 +28,27 @@
 
 <?php wp_footer(); ?>
 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-56568943-1', 'auto');
+  ga('send', 'pageview');
+</script>
+
+
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 964159523;
+var google_custom_params = window.google_tag_params;
+var google_remarketing_only = true;
+/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js"></script>
+<noscript>
+<div style="display:inline;"><img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/964159523/?value=0&amp;guid=ON&amp;script=0"/></div>
+</noscript>
+
 </body>
 </html>
